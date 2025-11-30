14afb6be11517d62edb804365d8ce47d9deac743
Andrew Lee <leeandrew135@gmail.com>
2025-11-30 02:04:23 -0500

    note about hash index implementation

--- src/HashIndex.py
+++ src/HashIndex.py
@@ -1,3 +1,6 @@
 # HashIndex.py
 # IMPLEMENT ONLY THE STRUCTURE OF A HASH INDEX,
 # AS THIS FILE WILL BE CALLED FROM main.py  
+
+# Before defining classes and methods, may be worth looking into using
+# a regular Python dict in place of implementing an entire hash index system.
