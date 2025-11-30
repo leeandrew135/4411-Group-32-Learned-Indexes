49824a856c70134afd7665ec5a22a38e54b8e621
Andrew Lee <leeandrew135@gmail.com>
2025-11-30 00:50:18 -0500

    basic functions for node class
    however still need to implement the functions of the B+ tree class

--- src/BPlusTree.py
+++ src/BPlusTree.py
@@ -1,3 +1,25 @@
 # BPlusTree.py
 # IMPLEMENT ONLY THE STRUCTURE OF B+ TREE,
 # AS THIS FILE WILL BE CALLED FROM main.py  
+
+class TreeNode:
+    def __init__(self, nodeOrder: int):
+        self.nodeOrder = nodeOrder
+        self.isLeaf = False
+        self.childNodes = []
+        self.parent = None
+
+    def setOrder(self, nodeOrder):
+        self.nodeOrder = nodeOrder
+
+    def setIsLeafTrue(self):
+        self.isLeaf = True
+    def setIsLeafFalse(self):
+        self.isLeaf = False
+
+    def addChildNode(self, childNode):
+        self.childNodes.append(childNode)
+
+    def setParentNode(self, parentNode):
+        self.parent = parentNode
+
