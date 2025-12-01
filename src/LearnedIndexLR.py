fba9c523145953ccbb3ef638bfba0c77cbc7630c
Andrew Lee <leeandrew135@gmail.com>
2025-12-01 13:19:32 -0500

    finished method for calculating LR slope + intercept

--- src/LearnedIndexLR.py
+++ src/LearnedIndexLR.py
@@ -21,20 +21,29 @@ class LearnedIndexLR:
 
         # CALCULATE AVERAGES FOR LINEAR REGRESSION FORMULA
         numberOfPoints = len(dataYears)
-        avgX = sum(dataYears) / numberOfPoints
-        avgY = sum(dataValues) / numberOfPoints
+        # x, y => years, values
+        avgYears = sum(dataYears) / numberOfPoints
+        avgValues = sum(dataValues) / numberOfPoints
 
         # CALCULATE THE SLOPE
-
+        numeratorSum = 0
+        denominatorSum = 0
+        for year, value in self.data:
+            xCalc = year - avgYears
+            numeratorSum += xCalc * (value - avgValues)
+            denominatorSum += xCalc * xCalc
+        self.m = numeratorSum / denominatorSum
 
         # CALCULATE THE INTERCEPT 
-
-
+        self.b = avgValues - self.m * avgYears
 
     # USE [year] TO PREDICT POSITION BY [mx + b]
     def predict(self, year: int):
         returnValue = self.m * year + self.b
         return returnValue
+    
+    def calculateErrorRanges(self):
+        pass
 
     def printModel(self):
         pass
