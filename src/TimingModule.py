ea1a15357f6b5094a9b99ec7729b6d46b90e272b
Andrew Lee <leeandrew135@gmail.com>
2025-12-08 20:17:00 -0500

    added function for repeated LR calls

--- src/TimingModule.py
+++ src/TimingModule.py
@@ -5,6 +5,60 @@ import time
 from LRManager import LRManager
 from HashIndex import HashIndex
 
+def timeLR(filepath, indexColumn): 
+    # CREATE MANAGER (NO NEED TO BE TIMED)
+    manager = LRManager(filepath, indexColumn)
+
+    # READS AND SORTS DATA, BUT DOES NOT CREATE/TRAIN MODEL
+    # WILL BE TIMED SINCE OTHER INDEXING IMPLEMENTATION NEEDS TO READ IN DATA
+    timeStart = time.time()
+    manager.processInputFile()
+    timeEnd = time.time()
+    resultingTime = timeEnd - timeStart
+    # print("PROCESS INPUT TIME: " + str(resultingTime) + " SECONDS.")
+    print("PROCESS INPUT TIME: " + str(resultingTime * 1000) + " ms.")
+
+    # CREATES AND TRAINS MODEL
+    timeStart = time.time()
+    manager.initModel()
+    timeEnd = time.time()
+    resultingTime = timeEnd - timeStart
+    # print("CREATE AND TRAIN MODEL TIME: " + str(resultingTime) + " SECONDS.")
+    print("CREATE AND TRAIN MODEL TIME: " + str(resultingTime * 1000) + " ms.")
+
+    # GET THE MODEL
+    # NO NEED TO BE TIMED(?)
+    model = manager.getModel()
+
+    # LOOKUP A KEY VALUE FROM THE CSV (1188 FOR TESTING)
+    timeStart = time.time()
+    model.getIndexPosition(1188)
+    timeEnd = time.time()
+    resultingTime1 = timeEnd - timeStart
+    # print("TIME TO LOOKUP KEY VALUE 1188: " + str(resultingTime) + " SECONDS.")
+    print("TIME TO LOOKUP KEY VALUE 1188: " + str(resultingTime * 1000) + " ms.")
+
+    # REMOVE A KNOWN KEY VALUE (1188), THEN READJUST MODEL
+    timeStart = time.time()
+    model.removeIndex(1188)
+    timeEnd = time.time()
+    resultingTime2 = timeEnd - timeStart
+    # print("TIME TO LOOKUP KEY VALUE 1188: " + str(resultingTime) + " SECONDS.")
+    print("TIME TO REMOVE KEY VALUE 1188: " + str(resultingTime * 1000) + " ms.")
+
+    # ADD BACK A KEY VALUE KNOWN TO NOT EXIST (1188), THEN READJUST MODEL
+    timeStart = time.time()
+    model.addIndex(1188)
+    timeEnd = time.time()
+    resultingTime3 = timeEnd - timeStart
+    # print("TIME TO LOOKUP KEY VALUE 1188: " + str(resultingTime) + " SECONDS.")
+    print("TIME TO INSERT KEY VALUE 1188: " + str(resultingTime * 1000) + " ms.")
+    
+    print("\n")
+
+    return [resultingTime1, resultingTime2, resultingTime3]
+
+
 def main():
     # GET PARAMS FROM COMMAND LINE
     indexMethod = sys.argv[1]
@@ -15,55 +69,28 @@ def main():
 
     # LINEAR REGRESSION
     if indexMethod == "LR":
-        # CREATE MANAGER (NO NEED TO BE TIMED)
-        manager = LRManager(filepath, indexColumn)
+        counter = 0
+        iterations = 500
+        totalLookup = 0
+        totalRemove = 0
+        totalInsert = 0
 
-        # READS AND SORTS DATA, BUT DOES NOT CREATE/TRAIN MODEL
-        # WILL BE TIMED SINCE OTHER INDEXING IMPLEMENTATION NEEDS TO READ IN DATA
-        timeStart = time.time()
-        manager.processInputFile()
-        timeEnd = time.time()
-        resultingTime = timeEnd - timeStart
-        # print("PROCESS INPUT TIME: " + str(resultingTime) + " SECONDS.")
-        print("PROCESS INPUT TIME: " + str(resultingTime * 1000) + " ms.")
+        while counter < iterations:
+            result = timeLR(filepath, indexColumn)
+            totalLookup += result[0]
+            totalRemove += result[1]
+            totalInsert += result[2]
+            counter += 1
 
-        # CREATES AND TRAINS MODEL
-        timeStart = time.time()
-        manager.initModel()
-        timeEnd = time.time()
-        resultingTime = timeEnd - timeStart
-        # print("CREATE AND TRAIN MODEL TIME: " + str(resultingTime) + " SECONDS.")
-        print("CREATE AND TRAIN MODEL TIME: " + str(resultingTime * 1000) + " ms.")
+        avgLookup = totalLookup / iterations
+        avgRemove= totalRemove / iterations
+        avgInsert = totalInsert / iterations
 
-        # GET THE MODEL
-        # NO NEED TO BE TIMED(?)
-        model = manager.getModel()
-
-        # LOOKUP A KEY VALUE FROM THE CSV (1188 FOR TESTING)
-        timeStart = time.time()
-        model.getIndexPosition(1188)
-        timeEnd = time.time()
-        resultingTime = timeEnd - timeStart
-        # print("TIME TO LOOKUP KEY VALUE 1188: " + str(resultingTime) + " SECONDS.")
-        print("TIME TO LOOKUP KEY VALUE 1188: " + str(resultingTime * 1000) + " ms.")
-
-        # REMOVE A KNOWN KEY VALUE (1188), THEN READJUST MODEL
-        timeStart = time.time()
-        model.removeIndex(1188)
-        timeEnd = time.time()
-        resultingTime = timeEnd - timeStart
-        # print("TIME TO LOOKUP KEY VALUE 1188: " + str(resultingTime) + " SECONDS.")
-        print("TIME TO REMOVE KEY VALUE 1188: " + str(resultingTime * 1000) + " ms.")
-
-        # ADD BACK A KEY VALUE KNOWN TO NOT EXIST (1188), THEN READJUST MODEL
-        timeStart = time.time()
-        model.addIndex(1188)
-        timeEnd = time.time()
-        resultingTime = timeEnd - timeStart
-        # print("TIME TO LOOKUP KEY VALUE 1188: " + str(resultingTime) + " SECONDS.")
-        print("TIME TO INSERT KEY VALUE 1188: " + str(resultingTime * 1000) + " ms.")
-        
+        print("AVG. LR LOOKUP OVER " + str(iterations) + ": " + str(avgLookup * 1000) + " ms.")
+        print("AVG. LR REMOVE OVER " + str(iterations) + ": " + str(avgRemove * 1000) + " ms.")
+        print("AVG. LR INSERT OVER " + str(iterations) + ": " + str(avgInsert * 1000) + " ms.")
         print("\n")
+
     # B+ TREE
     elif indexMethod == "BT":
         pass
@@ -84,11 +111,11 @@ def main():
         timeEnd = time.time()
         print("BUILD INDEX TIME: " + str((timeEnd - timeStart) * 1000) + " ms.")
 
-        # LOOKUP A KNOWN KEY VALUE (2000)
+        # LOOKUP A KNOWN KEY VALUE (1188)
         timeStart = time.time()
-        positions = hash_idx.getIndexPosition(2000) 
+        positions = hash_idx.getIndexPosition(1188) 
         timeEnd = time.time()
-        print("TIME TO LOOKUP KEY 2000: " + str((timeEnd - timeStart) * 1000) + " ms.")
+        print("TIME TO LOOKUP KEY 1188: " + str((timeEnd - timeStart) * 1000) + " ms.")
         
         # VERIFY SEARCH RESULTS
         if positions:
@@ -96,11 +123,18 @@ def main():
         else:
             print("   > Key not found.")
         
-        # REMOVE THE KEY (2000) FROM THE INDEX
+        # REMOVE THE KEY (1188) FROM THE INDEX
         timeStart = time.time()
-        hash_idx.removeIndex(2000)
+        hash_idx.removeIndex(1188)
         timeEnd = time.time()
-        print("TIME TO REMOVE KEY 2000: " + str((timeEnd - timeStart) * 1000) + " ms.")
+        print("TIME TO REMOVE KEY 1188: " + str((timeEnd - timeStart) * 1000) + " ms.")
+
+        # ADD BACK A KEY VALUE KNOWN TO NOT EXIST (1188)
+        timeStart = time.time()
+        hash_idx.addIndex(1188)
+        timeEnd = time.time()
+        resultingTime = timeEnd - timeStart
+        print("TIME TO INSERT KEY VALUE 1188: " + str(resultingTime * 1000) + " ms.")
 
         print("\n")
     # NEURAL NETWORK
